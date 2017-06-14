b3b7c7b4c55e213f713137212d9197af92d6e71b
Jack Kerigan <support@kerigan.com>
2017-06-14 18:03:30 -0400

    finished header styles

--- wp-content/themes/gcrp/header.php
+++ wp-content/themes/gcrp/header.php
@@ -24,7 +24,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'gcrp' ); ?></a>
 
-    <div class="hidden-md-up">
+    <div class="hidden-lg-up">
         <div class="collapse navbar-toggleable-sm justify-content-center" id="mobile-nav">
 			<?php wp_nav_menu(
 				array(
@@ -44,12 +44,12 @@
         <header id="masthead" class="site-header">
             <div class="navbar-static-top navbar-transparent">
                 <div class="container">
-                    <div class="row justify-content-center d-flex align-items-end">
+                    <div class="row justify-content-center d-flex align-items-end no-gutters">
 
-                        <div class="col-9 col-md-4">
-                            <a href="/" class="navbar-brand"><img src="<?php echo get_template_directory_uri().'/img/gcrp-logo.jpg'; ?>" alt="<?php echo get_bloginfo(); ?>" class="img-fluid"></a>
+                        <div class="col-10 col-sm-9 col-md-6 col-lg-5 align-self-start">
+                            <a href="/" class="navbar-brand"><img src="<?php echo get_template_directory_uri().'/img/gcrp-logo.jpg'; ?>" alt="<?php echo get_bloginfo(); ?>" class="img-fluid mt-lg-2"></a>
                         </div>
-                        <div class="col-3 hidden-md-up">
+                        <div class="col-2 col-sm-3 text-center hidden-lg-up">
                             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
                                 <span class="icon-box">
                                     <span class="icon-bar"></span>
@@ -58,27 +58,30 @@
                                 </span>
                             </button>
                         </div>
-                        <div class="col-md-6 hidden-sm-down">
-                            <div id="mini-nav" class="align-self-end">
+                        <div class="col-md-3 col-lg-7">
+                            <div id="mini-nav" class="justify-content-start" >
 		                        <?php wp_nav_menu(
 			                        array(
 				                        'theme_location'  => 'mini-menu',
-				                        'container_class' => 'navbar',
+				                        'container_class' => 'navbar hidden-md-down d-inline-flex',
 				                        'container_id'    => 'navbarNav',
-				                        'menu_class'      => 'navbar-nav mr-auto',
+				                        'menu_class'      => 'navbar-nav mr-auto mt-5',
 				                        'fallback_cb'     => '',
 				                        'menu_id'         => 'mini-menu',
 				                        'walker'          => new WP_Bootstrap_Navwalker(),
 			                        )
 		                        ); ?>
+                                <div class="secondary-logo hidden-sm-down d-inline-flex float-right">
+                                    <img src="<?php echo get_template_directory_uri().'/img/min-logo.jpg'; ?>" alt="<?php echo get_bloginfo(); ?>" class="img-fluid">
+                                </div>
                             </div>
-                            <div id="main-nav" class="hidden-md-down">
+                            <div id="main-nav" >
 			                    <?php wp_nav_menu(
 				                    array(
 					                    'theme_location'  => 'main-menu',
-					                    'container_class' => 'navbar',
+					                    'container_class' => 'navbar d-flex ml-auto hidden-md-down',
 					                    'container_id'    => 'navbarNavDropdown',
-					                    'menu_class'      => 'navbar-nav ml-auto',
+					                    'menu_class'      => 'navbar-nav ml-auto text-right',
 					                    'fallback_cb'     => '',
 					                    'menu_id'         => 'main-menu',
 					                    'walker'          => new WP_Bootstrap_Navwalker(),
@@ -86,27 +89,6 @@
 			                    ); ?>
                             </div>
                         </div>
-                        <div class="col-md-2 hidden-sm-down">
-                            <div class="secondary-logo">
-                                <img src="<?php echo get_template_directory_uri().'/img/min-logo.jpg'; ?>" alt="<?php echo get_bloginfo(); ?>" class="img-fluid">
-                            </div>
-                        </div>
-                        <div id="main-nav" class="hidden-lg-up hidden-sm-down col-12">
-		                    <?php wp_nav_menu(
-			                    array(
-				                    'theme_location'  => 'main-menu',
-				                    'container_class' => 'navbar',
-				                    'container_id'    => 'navbarNavDropdown',
-				                    'menu_class'      => 'navbar-nav ml-auto',
-				                    'fallback_cb'     => '',
-				                    'menu_id'         => 'main-menu',
-				                    'walker'          => new WP_Bootstrap_Navwalker(),
-			                    )
-		                    ); ?>
-                        </div>
-
-
-
                     </div>
                 </div>
             </div>
