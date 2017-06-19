02205ca380b5735d64831459ad6390b030ea8224
Jack Kerigan <support@kerigan.com>
2017-06-19 13:57:20 -0400

    completed precinct map pages.

--- wp-content/themes/gcrp/template-parts/content-page.php
+++ wp-content/themes/gcrp/template-parts/content-page.php
@@ -16,8 +16,15 @@
 			the_content();
 
             if ( $post->post_parent == '8' || is_page(8) ) {
-	            $map = get_the_post_thumbnail(null,'post-thumbnail');
-	            echo '<pre>',print_r($map),'</pre>';
+	            $map = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' );
+	            //echo '<pre>',print_r($map),'</pre>';
+	            echo '<a href="'.$map[0].'" class="btn btn-primary mb-2 mr-2 lightbox-link" data-lightbox="lightbox" >Open voting precinct map<a/>';
+	            echo '<a class="btn mb-2 mr-2 btn-primary" href="http://www.votegulf.com/Voting-Information/Voter-Information-Lookup" target="_blank" >Look up voting information on votegulf.com</a>';
+
+	            wp_enqueue_script( 'lightbox' );
+	            wp_enqueue_style( 'lightbox-styles' );
+	            add_action( 'wp_footer', 'lightbox_to_footer',100 );
+
             }
 
 		?>
